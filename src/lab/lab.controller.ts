f3e1330c1befe90b149b74b242191694ea128d46
Stanislav <future.12@yandex.ru>
2020-04-18 19:21:32 +0300

    added lab

---
+++ src/lab/lab.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, HttpCode } from '@nestjs/common'
+
+@Controller('lab')
+export class LabController {
+  @Get('success')
+  @HttpCode(200)
+  getSuccess() {
+    return 'all right'
+  }
+
+  @Get('delay')
+  @HttpCode(200)
+  async getWithDelay() {
+    await new Promise(res => setTimeout(res, 5000))
+    return 'response with delay'
+  }
+  @Get('error')
+  @HttpCode(400)
+  getError() {
+    return 'response with error'
+  }
+}
---
+++ src/lab/lab.module.ts
@@ -0,0 +1,9 @@
+import { LabController } from './lab.controller'
+import { Module } from '@nestjs/common'
+
+@Module({
+  imports: [],
+  controllers: [LabController],
+  providers: [],
+})
+export class LabModule {}
