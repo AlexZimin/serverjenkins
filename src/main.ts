57034abea7e0573fd91faa265167e021ff7da844
alex <alexzimin.std@gmail.com>
2020-05-02 00:41:56 +0300

    tst

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT || 1337, (port) => console.log('started ', port)
-  )
+  await app.listen(process.env.PORT || 1337, () => console.log('started '))
+
 }
 bootstrap()
