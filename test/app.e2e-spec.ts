d3d3b0283538cfbb0145f484d544d05207427a8c
Alex <babaytseff@yandex.ru>
2020-05-02 15:51:44 +0300

    Update app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,7 +19,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/lab/success')
       .expect(200)
-      .expect('all right')
+      .expect('all right!!!!!!!!')
   })
   it('should return error /lab/error (GET)', () => {
     return request(app.getHttpServer())
